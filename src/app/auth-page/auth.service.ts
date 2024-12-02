32133249a10626bc83578730f20600b42c6a6712
ansar <ansarsaudagar40@gmail.com>
2024-12-02 23:11:49 +0530

    added code for jwt in auth service

--- src/app/auth-page/auth.service.ts
+++ src/app/auth-page/auth.service.ts
@@ -3,7 +3,8 @@ import { environment } from '../../environments/environment.development';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../models/user.model';
 import { LoginCredentials, LoginResponse } from '../models/login.model';
-
+import { BehaviorSubject, tap } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -13,13 +14,70 @@ export class AuthService {
   private LOGIN_URL = this.BASE_URL + 'login';
   private SIGNUP_URL = this.BASE_URL + 'signup';
 
-  constructor(private http: HttpClient) {}
+  private tokenExpirationTimer: any;
+  private readonly TOKEN_KEY = 'authToken';
+
+  user = new BehaviorSubject<User | null>(null);
+
+  constructor(private http: HttpClient, private router: Router) {}
 
   signup(user: User) {
     return this.http.post<User>(this.SIGNUP_URL, user);
   }
 
-  login(credentials : LoginCredentials){
-    return this.http.post<LoginResponse>(this.LOGIN_URL, credentials);
+  login(credentials: LoginCredentials) {
+    return this.http.post<LoginResponse>(this.LOGIN_URL, credentials).pipe(
+      tap((response: LoginResponse) => {
+        this.handleAuthentication(response.token);
+      })
+    );
+  }
+
+  autoLogin() {
+    const token = this.getToken();
+    if (!token) return;
+
+    const decodedToken = this.decodeToken(token);
+    const expirationDate = new Date(decodedToken.exp * 1000);
+
+    if (expirationDate > new Date()) {
+      this.user.next({ email: decodedToken.email } as User);
+      this.autoLogout(decodedToken.exp * 1000 - Date.now());
+    } else {
+      this.logout();
+    }
+  }
+
+  logout() {
+    localStorage.removeItem(this.TOKEN_KEY);
+    this.user.next(null);
+    clearTimeout(this.tokenExpirationTimer);
+    this.router.navigateByUrl('/auth/login');
+  }
+
+  autoLogout(expirationDuration: number) {
+    this.tokenExpirationTimer = setTimeout(() => {
+      this.logout();
+    }, expirationDuration);
+  }
+
+  private handleAuthentication(token: string) {
+    const decodedToken = this.decodeToken(token);
+    const expirationTime = new Date(decodedToken.exp * 1000);
+
+    localStorage.setItem(this.TOKEN_KEY, token);
+
+    this.user.next({ email: decodedToken.email } as User);
+
+    this.autoLogout(decodedToken.exp * 1000 - Date.now());
+  }
+
+  private decodeToken(token: string): any {
+    const payload = token.split('.')[1];
+    return JSON.parse(atob(payload));
+  }
+
+  getToken() {
+    return localStorage.getItem(this.TOKEN_KEY);
   }
 }
