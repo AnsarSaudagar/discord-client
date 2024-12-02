3dd378acb667a2bdbc64399a98ad694a0b690065
ansar <ansarsaudagar40@gmail.com>
2024-12-02 10:50:34 +0530

    reactive form and basic validation added to login form

--- src/app/auth-page/login/login.component.ts
+++ src/app/auth-page/login/login.component.ts
@@ -3,11 +3,18 @@ import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { LoginCredentials } from '../../models/login.model';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule, HttpClientModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
   host: {
@@ -15,13 +22,36 @@ import { AuthService } from '../auth.service';
   },
 })
 export class LoginComponent {
-  constructor(private router: Router, private authService: AuthService) {}
+  loginForm: FormGroup;
+  isSubmitted: boolean = false;
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private fb: FormBuilder
+  ) {
+    this.loginForm = fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
 
   onClickRegister() {
     this.router.navigate(['auth', 'signup']);
   }
-  
-  onClickLogin(){
 
+  onClickLogin() {
+    this.isSubmitted = true;
+    if(this.loginForm.valid){
+      const formValues : LoginCredentials = this.loginForm.value;
+      console.log(formValues); 
+    }
+  }
+
+  getValidationCondition(input_name: string) {
+    return (
+      (this.loginForm.get(input_name)?.errors &&
+        this.loginForm.get(input_name)?.touched) ||
+      (this.loginForm.get(input_name)?.errors && this.isSubmitted)
+    );
   }
 }
