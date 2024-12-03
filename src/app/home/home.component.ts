5ed208aae7d279b41f0efe12861fd2b189da50e6
ansar <ansarsaudagar40@gmail.com>
2024-12-03 22:48:09 +0530

    main sidebar comp generated and main icon addedf

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../auth-page/auth.service';
+import { MainSidebarComponent } from '../main-sidebar/main-sidebar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MainSidebarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
