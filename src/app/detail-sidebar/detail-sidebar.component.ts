e6b193f98377e4802d1d9c52925077c8cdf575c3
ansar <ansarsaudagar40@gmail.com>
2024-12-03 23:12:49 +0530

    working on detail sidebar

---
+++ src/app/detail-sidebar/detail-sidebar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-sidebar',
+  standalone: true,
+  imports: [],
+  templateUrl: './detail-sidebar.component.html',
+  styleUrl: './detail-sidebar.component.css'
+})
+export class DetailSidebarComponent {
+
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../auth-page/auth.service';
 import { MainSidebarComponent } from '../main-sidebar/main-sidebar.component';
+import { DetailSidebarComponent } from '../detail-sidebar/detail-sidebar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MainSidebarComponent],
+  imports: [MainSidebarComponent, DetailSidebarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
