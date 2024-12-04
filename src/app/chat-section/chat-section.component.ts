4b43fb5ed14f102a0cc0285d4d76d3c779f18bb4
ansar <ansarsaudagar40@gmail.com>
2024-12-04 19:26:35 +0530

    error ui handling for friend input done

--- src/app/chat-section/chat-section.component.ts
+++ src/app/chat-section/chat-section.component.ts
@@ -17,19 +17,35 @@ import { FriendRequestResponse } from '../models/friend_request_response.model';
 export class ChatSectionComponent {
   focusInput: boolean = false;
   friendInput: string = '';
+  successRequest : boolean= false;
+  errorRequest : boolean = false;
+  showMessage : string = "";
 
   // success  #21A559
   // Success! Your friend request to ansar.dev_56399  was sent.
 
   // error #F23F43
   // Hm, that didn’t work. Double-check that the username is correct.
+
+  // error text #FA767A
   constructor(private friendService: FriendService) {}
 
   onClickFriendRequest() {
     this.friendService.sendFriendRequest(this.friendInput).subscribe({
       next: (res: FriendRequestResponse) => {
-        console.log(res);
+        this.successRequest = true;
+        this.showMessage = `Success! Your friend request to ${this.friendInput}  was sent`;
       },
+      error: (err) => {
+          this.errorRequest = true;
+          this.showMessage = `Hm, that didn’t work. Double-check that the username is correct.`;
+      }
     });
+  }
+
+  onChangeFriendInput(){
+    this.showMessage = "";
+    this.successRequest = false;
+    this.errorRequest = false;
   }
 }
