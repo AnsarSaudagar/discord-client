43fae2593f323194b529899979cd7aa2ad419c40
ansar <ansarsaudagar40@gmail.com>
2024-12-08 00:01:33 +0530

    generated component for chat and gave basic layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,4 +9,5 @@ import { HOME_ROUTES } from './home/home.routes';
 export const routes: Routes = [
     {path: "auth", component: AuthPageComponent, children: AUTH_ROUTES},
     {path: "home", component: HomeComponent, canActivate: [authGuard], children: HOME_ROUTES},
+    {path: "**", redirectTo: 'home', pathMatch: "full"}
 ];
--- src/app/home/home.routes.ts
+++ src/app/home/home.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { FriendsComponent } from '../friends/friends.component';
 import { FRIEND_ROUTES } from '../friends/friends.routes';
+import { ChatComponent } from '../chat/chat.component';
 export const HOME_ROUTES: Routes = [
   { path: 'friends', component: FriendsComponent, children: FRIEND_ROUTES},
   { path: '', redirectTo: 'friends' ,pathMatch: "full"},
+  {path: "chat", component: ChatComponent},
+  { path: '**', redirectTo: 'friends' ,pathMatch: "full"},
 ];
